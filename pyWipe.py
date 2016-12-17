254823c110e7b62d8ffdf963f83f2bb97c9ef7d8
marshki <marshall@morethantomorrow.org>
2016-12-16 22:05:22 -0500

    Outline for program

---
+++ pyWipe.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3 
+
+"""
+Python program for filling hard drives with either zeroes (0s) or ones (1s). 
+"""
+
+# Import OS functions 
+
+# Zeroes to the drive 
+
+# Pseudo-random stuff to the drive 
+
+# Check OS for POSIX
+
+# Main function 
+
+
