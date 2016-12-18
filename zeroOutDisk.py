c8143c9f3eca422e48625700aeef11e528131caf
marshki <marshall@morethantomorrow.org>
2016-12-18 11:03:18 -0500

    add zeroOutDisk.py

---
+++ zeroOutDisk.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python 
+
+def zeroOutDisk():
+        """Fill selected device (/dev/) with zeros."""
+	pass=1 
+	for int in range(wipe):
+		print 'Wiping drives pass %s of $s")%(pass, count))
+		os.system(("dd if=/dev/zero of=%s")%(device))
+		pass+=1
+
