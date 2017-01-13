3534cf4a965d9c12fac7fb9ecb287b9c8b44cf5c
marshki <marshall@morethantomorrow.org>
2017-01-13 13:20:51 -0500

    fix sapcing issue

--- pyWipe.py
+++ pyWipe.py
@@ -7,7 +7,8 @@ def user_float():
     ''' Prompt user for input, accepting only valid input.'''
     while True: 
         try: 
-            return float(input('How many times would you like to wipe this device?'))
+            return float(input('How many times would you like to wipe this device?: '))
         except ValueError: 
             print('Bad value, try again.')
- 
+
+user_float() 
