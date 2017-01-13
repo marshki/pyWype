1d3dd9d9d4db5f6be73ba70fa74f88ade0cf7f7e
marshki <marshall@morethantomorrow.org>
2017-01-13 13:18:36 -0500

    User input for wipe #s

--- pyWipe.py
+++ pyWipe.py
@@ -2,4 +2,12 @@ from builtins import input
 
 #!/usr/bin/env Python3
 # Python 2 & 3 
+
+def user_float(): 
+    ''' Prompt user for input, accepting only valid input.'''
+    while True: 
+        try: 
+            return float(input('How many times would you like to wipe this device?'))
+        except ValueError: 
+            print('Bad value, try again.')
  
