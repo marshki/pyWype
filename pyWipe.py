9bee194123e364052806951f5e2c3eed18f78c9f
marshki <marshall@morethantomorrow.org>
2017-01-18 14:52:10 -0500

    Even more pseudo code

--- pyWipe.py
+++ pyWipe.py
@@ -4,7 +4,7 @@ from builtins import input
 # Python 2 & 3 
 
 ''' 
-Python program to fill storage device(s) with either zeros(0s), or a pseudrandom combination of zeros(0s) and ones (1s). 
+Python program to fill storage device(s) with either zeros(0s), or a pseudorandom combination of zeros(0s) and ones (1s). 
 '''
 
 # Import os function to provide Python with operating system-dependent functionality
@@ -33,4 +33,13 @@ def randomDisk():
 '''Fill disk with random sequence of zeros (0s) and (1s).'''
     return os.system('dd if=/dev/random')
 
-
+def menu(): 
+    '''Menu prompt for user to select program option.'''
+    while True: 
+        print('1. I want to fill a disk\(s) with zeros\(0s)')
+        print('2. I want to fill a disk\(s) with a pseudo-random sequence of zeros\(0s) and ones\(1s)')
+        print('3. I want to quit.')
+        print()
+        choice = input('Select an option (1, 2, or 3): ')
+        if choice in ('1', '2', '3'): 
+            return choice 
