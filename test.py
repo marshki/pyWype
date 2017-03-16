3bdec9f7c8cc3ca387b794133d5905806287dbcc
marshki <marshall@morethantomorrow.org>
2017-03-15 20:04:46 -0400

    fixed function

--- test.py
+++ test.py
@@ -6,8 +6,9 @@ def stringCheck():
     """ ABC check """                                                                                                                 
     while True:                                                                                                                     
         try:
-            stringy =  str(raw_input("Enter block device: ")) 
-            if not re.match("^[a-z]*$", stringy):                    
+            stringy =  str(raw_input("Enter block device: "))
+            # if len(stringy) != 1:  
+            if not re.match("^[a-z]*$", stringy) or len(stringy) != 1:                 
                 raise ValueError()
             return stringy 
         except ValueError:
