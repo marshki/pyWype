e092397014b9f93924e734c1ff0cb301fbbe5bba
marshki <marshall@morethantomorrow.org>
2017-03-19 18:21:31 -0400

    zero.py breaks a pipe, need to fix

--- zero.py
+++ zero.py
@@ -7,7 +7,7 @@ def zeroToDrive():
     ''' write zeros to drive '''
     wipes = 1 
     for int in range(count): 
-        os.system(("dd if=/dev/zero of=/dev/null"))
+        os.system(("dd if=/dev/zero | pv | of=/dev/null bs=4096"))
         wipes+=1 
 zeroToDrive()
         
