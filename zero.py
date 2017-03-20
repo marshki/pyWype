6cef35b059a4c2e7c4b20fa486a03a5f61e51011
marshki <marshall@morethantomorrow.org>
2017-03-20 18:53:04 -0400

    pv with ddwq

--- zero.py
+++ zero.py
@@ -1,13 +1,13 @@
 #!/bin/py 
 import os 
 
-count = 2 
+count = 1 
 
 def zeroToDrive():
     ''' write zeros to drive '''
     wipes = 1 
     for int in range(count): 
-        os.system(("dd if=/dev/zero bs=4096 | pv | dd of=/dev/null"))
+        os.system(("dd if=/dev/zero bs=4096 | pv --progress --timer --rate --bytes| dd of=/dev/null bs=4096"))
+        # os.system(os.system(("dd if=/dev/zero bs=4096 | pv -ptrb | dd of=/dev/null bs=4096"))
         wipes+=1 
 zeroToDrive()
-        
