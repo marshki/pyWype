9e16ae019f22d80bcd7774c2e7099479db477985
marshki <marshall@morethantomorrow.org>
2017-03-20 17:47:44 -0400

    got true real time monitoring for this function

--- zero.py
+++ zero.py
@@ -7,7 +7,7 @@ def zeroToDrive():
     ''' write zeros to drive '''
     wipes = 1 
     for int in range(count): 
-        os.system(("dd if=/dev/zero | pv | of=/dev/null bs=4096"))
+        os.system(("dd if=/dev/zero bs=4096 | pv | dd of=/dev/null"))
         wipes+=1 
 zeroToDrive()
         
