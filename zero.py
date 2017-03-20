1e362ab8704c76e3606fae9317dd85eeb06259ea
marshki <marshall@morethantomorrow.org>
2017-03-20 18:59:46 -0400

    remove superflous block size

--- zero.py
+++ zero.py
@@ -7,7 +7,7 @@ def zeroToDrive():
     ''' write zeros to drive '''
     wipes = 1 
     for int in range(count): 
-        os.system(("dd if=/dev/zero bs=4096 | pv --progress --timer --rate --bytes| dd of=/dev/null bs=4096"))
-        # os.system(os.system(("dd if=/dev/zero bs=4096 | pv -ptrb | dd of=/dev/null bs=4096"))
+        os.system(("dd if=/dev/zero |pv --progress --timer --rate --bytes| dd of=/dev/null bs=4096"))
+        # os.system(os.system(("dd if=/dev/zero| pv -ptrb | dd of=/dev/null bs=4096"))
         wipes+=1 
 zeroToDrive()
