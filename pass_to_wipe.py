77bb78243faf7cab9b1c2df4c08a5f4ae628d76c
marshki <marshall@morethantomorrow.org>
2017-03-24 11:32:03 -0400

    testing functions

---
+++ pass_to_wipe.py
@@ -0,0 +1,26 @@
+#!/bin/py 
+# testing 
+
+import os 
+
+def numberOfWipes(): 
+    """ Prompt user for number of wipes to perform """ 
+    while True: 
+        try:
+            wipes = int(raw_input('How many times do you want to wipe the disk?: '))
+            if not wipes > 0: 
+                raise ValueError()
+            return wipes 
+        except ValueError: 
+            print "Sorry, that\'s not a valid number. Please try again."
+
+def zerosToDrive():
+    """ Write zeros to drive """
+    int = numberOfWipes()
+    print int 
+    wipes = 1
+    for int in range(1):#(numberOfWipes): 
+        os.system(("dd if=/dev/zero |pv --progress --time --rate --bytes| dd of=/dev/null bs=4096")) # pv -ptrb         
+        wipes+=1
+
+zerosToDrive()
