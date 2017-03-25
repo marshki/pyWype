185ee533df81f5032e9bfb840a82a4fb0edaddee
marshki <marshall@morethantomorrow.org>
2017-03-25 13:14:36 -0400

    testing for append.py

--- append.py
+++ append.py
@@ -16,8 +16,34 @@ def defineBlockDevice():
 def appendBlockDevice(): 
     """ Append user-defined block device to /dev/sd """ 
     letter = defineBlockDevice()
-    return '/dev/sd' + letter
+    return '/dev/sd' + letter 
 
-appendBlockDevice()
+def numberOfWipes():                                                                                                                  
+    """ Prompt user for number of wipes to perform """                                                                                
+    while True:                                                                                                                       
+        try:                                                                                                                          
+            wipes = int(raw_input('How many times do you want to wipe the disk?: '))                                                  
+            #if not wipes > 0:                                                                                                         
+            #    raise ValueError()                                                                                                    
+            return wipes                                                                                                              
+        except ValueError:                                                                                                            
+            print 'Sorry, that\'s not a valid number. Please try again.'     
+
+
+def zerosToDrive():                                                                                                                   
+    """ Write zeros to drive """
+                                                                                                          
+    num = numberOfWipes()                                                                                                             
+    dev = appendBlockDevice()
+
+    print dev 
+                                                                                                                                  
+    passes = 1                                                                                                                        
+    for int in range(num):                                                                                                            
+        print 'Processing pass count %s of %d ... '%(passes, num)                                                             
+        os.system(('dd if=/dev/zero |pv --progress --time --rate --bytes| dd of=dev bs=4096')) # pv -ptrb                       
+        passes+=1         
+
+zerosToDrive()
 
 
