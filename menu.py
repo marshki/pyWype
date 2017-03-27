9b35f5a54d273e2fd377390c616faa5dd818f71c
marshki <marshall@morethantomorrow.org>
2017-03-27 19:21:13 -0400

    Update menu options to reflect OS X Disk Utility: secureErase

--- menu.py
+++ menu.py
@@ -5,15 +5,15 @@
 def menu(): 
     """ Menu prompt for user to select program option """
     while True: 
-        print 'I'
-        print 'II'
-        print 'III' 
-        print 'IV'
-        print 'V'
+        print '0 - Single-pass zero-fill erase.'
+        print '1 - Single-pass random-fill erase.'
+        print '2 - US DoD 7-pass secure erase.' 
+        print '3 - Gutman algorithm 35-pass secure erase.'
+        print '4.- US DoE algorithm 3-pass secure erase.'
 
         choice = raw_input('Select an option (I, II, III, IV, V): ') 
 
-        if choice in ('I', 'II', 'III', 'IV', 'V'): 
+        if choice in ('0', '1', '2', '3', '4'): 
             return choice 
 
 menu()
