ae8565ae9197167629a157b122d1666b57291529
marshki <marshall@morethantomorrow.org>
2017-03-28 11:41:57 -0400

    Place holders for wiping algorithms.

---
+++ wipeAlgos.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python 
+
+'''
+
+def singlePassZero(): 
+    """ Single-pass zero-fill erase """
+
+def singlePassRandom(): 
+    """ Single-pass random-fill erase """ 
+
+def usDoD7Pass(): 
+    """ US DoD 7-pass secure erase """ 
+
+def gutmann35Pass(): 
+    """ Gutman algorithm 35-pass secure erase """ 
+
+def usDoE3Pass(): 
+    """ US DoE algorithm 3-pass secure erase """ 
+
+'''
