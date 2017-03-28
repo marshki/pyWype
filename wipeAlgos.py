2ed7b07cd110f4c3c788ded5c88bfa32fd41c096
marshki <marshall@morethantomorrow.org>
2017-03-28 11:51:34 -0400

    add two wipe algorithms

--- wipeAlgos.py
+++ wipeAlgos.py
@@ -1,14 +1,37 @@
 #!/usr/bin/env python 
 
-'''
 
 def singlePassZero(): 
     """ Single-pass zero-fill erase """
 
+    num = numberOfWipes()
+    confirm = confirmWipe()
+
+    passes = 1
+
+    for int in range(num):
+        print 'Processing pass count %s of %d ... '%(passes, num)
+        os.system(('dd if=/dev/zero |pv --progress --time --rate --bytes| dd of=/dev/null bs=4096')) # pv -ptrb
+        passes += 1
+
 def singlePassRandom(): 
     """ Single-pass random-fill erase """ 
 
-def usDoD7Pass(): 
+    num = numberOfWipes()
+    confirm = confirmWipe()
+
+    passes = 1
+
+    for int in range(num):
+        print 'Processing pass count %s of %d ...'%(passes, num)
+        os.system(('dd if=/dev/random |pv --progress --time --rate --bytes| dd of=/dev/null bs=4096')) # pv -ptrb
+        passes += 1
+
+
+
+
+ 
+def usDoD7Pass():
     """ US DoD 7-pass secure erase """ 
 
 def gutmann35Pass(): 
@@ -17,4 +40,3 @@ def gutmann35Pass():
 def usDoE3Pass(): 
     """ US DoE algorithm 3-pass secure erase """ 
 
-'''
