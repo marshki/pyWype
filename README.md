4fffbcf8542b58d52dc7ecaab9dfd5cd43008e4a
marshki <marshall@morethantomorrow.org>
2017-03-28 11:19:13 -0400

    cosmetic updates to README.md & printTest.py

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## pyWipe
 
-A text-based tool to securely wipe hard drives written to work with Python 2 & 3.
+A text-based tool to securely wipe hard drives written to work with Python 2 (and eventually Python 3).
 
 *diskWipe.py* works as is in Python 2.7 
 The idea here is to enhance the program in Python 2.7 first, then add support for Python 3.4. Eventually, it would be nice if this program could functionally do what OS X's Disk Utility--secure erase option--does: 
--- menu.py
+++ menu.py
@@ -4,6 +4,7 @@
 
 def menu(): 
     """ Menu prompt for user to select program option """
+
     while True: 
         print '0 - Single-pass zero-fill erase.'
         print '1 - Single-pass random-fill erase.'
@@ -11,7 +12,7 @@ def menu():
         print '3 - Gutman algorithm 35-pass secure erase.'
         print '4.- US DoE algorithm 3-pass secure erase.'
 
-        choice = raw_input('Select an option (I, II, III, IV, V): ') 
+        choice = raw_input('Select an option (0, 1, 2, 3, 4): ') 
 
         if choice in ('0', '1', '2', '3', '4'): 
             return choice 
