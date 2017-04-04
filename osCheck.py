eabeaac3f0dbefd51543167a1a668d5e40e7c1b2
marshki <marshall@morethantomorrow.org>
2017-04-04 11:49:57 -0400

    change to sys.exit()

--- osCheck.py
+++ osCheck.py
@@ -7,7 +7,7 @@ def osCheck():
     """ Check if system is running 'Linux' """
     if not sys.platform.startswith('linux'): 
         print 'Sorry, we can\'t continue. Ciao' 
-        exit()
+        sys.exit()
 
 osCheck()
 
