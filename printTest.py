15691f5e041d6b9d560f4f7c3ba71d9563bfb9ac
marshki <marshall@morethantomorrow.org>
2017-04-06 10:32:47 -0400

    add menu function

--- printTest.py
+++ printTest.py
@@ -101,12 +101,26 @@ def randomToDrive():
         os.system(('dd if=/dev/random |pv --progress --time --rate --bytes| dd of=/dev/null bs=4096')) # pv -ptrb 
         passes += 1 
 
+def menu(): 
+    """ Menu prompt for use to select program option """ 
+    while True: 
+        print '1. Overwrite all sectors with zeros (Faster, less secure).'
+        print '2. Overwrite all sectors with random data (Slower, more secure).'
+        print '3. I want to quit.' 
+        print '' 
+        choice = raw_input('Select an option (1, 2 or 3): ')
+        if choice in ('1', '2', '3'): 
+            return choice 
+
+
 def wipeDrive():
     """ Prorgram to Wipe drive """ 
     
     osCheck()
     device = appendBlockDevice()  
     zero = zerosToDrive()
+    
 
+    menu()
 if __name__ == '__main__': 
     wipeDrive()
