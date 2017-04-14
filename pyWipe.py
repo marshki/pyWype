cb28c2670dc036094fa2edff5bf0aef1c24f41ab
marshki <marshall@morethantomorrow.org>
2017-04-14 11:34:49 -0400

    update regexp to check for one of two conditions

--- pyWipe.py
+++ pyWipe.py
@@ -31,7 +31,7 @@ def defineBlockDevice():
         try: 
             blockdevice = str(raw_input('Enter letter of block device to be wiped, e.g. to wipe \'/dev/sdb\' enter \'b\': '))  
 
-            if not re.match("^[a-z]$", blockdevice):                                       
+            if not re.match("^[a-z]$|^[a-z]\d$", blockdevice):                                       
                 raise ValueError()
             return blockdevice
 
