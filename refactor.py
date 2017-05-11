e50f40758a34949e3b697af1a4289fdb7f8da966
marshki <marshall@morethantomorrow.org>
2017-05-11 19:59:29 -0400

    refactor code to: present devices->define devices->define type of wipe->number of wipes

--- refactor.py
+++ refactor.py
@@ -15,27 +15,33 @@ import re               # For regular expression parsing
 
 """ Define functions """
 
-def osCheck():
-    """ Check if OS is 'Linux' """
-    if not sys.platform.startswith('linux'):
-        print('This program was designed for Linux. Exiting.') 
-        sys.exit()
+#def osCheck():
+#    """ Check if OS is 'Linux' """
+#    if not sys.platform.startswith('linux'):
+#        print('This program was designed for Linux. Exiting.') 
+#        sys.exit()
 
-def devPartHeader():
+def devicesHeader():
     """ Header for attached device(s) / partition(s) """
     print(24 * "-", "ATTACHED DEVICES", 24 * "-")
 
-def listDevPart(): 
+def listDevices(): 
     """ List mounted device(s) / partition(s) """
     
-    header = devPartHeader()
+    header = devicesHeader()
 
     return os.system('lsblk --nodeps --output NAME,MODEL,VENDOR,SIZE,STATE')      #lsblk -d -o NAME,MODEL,VENDOR,SIZE,STATE 
 
-def defineDevPart(): 
-    """ Prompt user to define device or partition to wipe """
+def defineDeviceHeader():
+    """ Header for device(s) / partiton(s)  definition """
+    print(25 * "-", "SELECT DEVICE", 25 * "-")
 
-    devpart = listDevPart() 
+def defineDevice(): 
+    """ Prompt user to define device or partition to wipe """
+    
+    devices = listDevices() 
+    
+    header = defineDeviceHeader()
 
     while True:
         try: 
@@ -48,13 +54,58 @@ def defineDevPart():
         except ValueError: 
             print('Sorry, that\'s not a valid device / partition. Try again.')
  
-def appendDevPart(): 
+def appendDevice(): 
     """ Append user-defined device/partition to /dev/sd """
     
-    letter = defineDevPart()
+    letter = defineDevice()
     
-    return '/dev/sd' + letter 
+    return '/dev/sd' + letter
 
+def menu(): 
+    """ Menu prompt for use to select program option """ 
+
+    append = appendDevice()    
+
+    while True: 
+        print(30 * "-", "SELECT ACTION", 30 * "-")
+        print('1. Overwrite all sectors with zeros (Faster, less secure).')
+        print('2. Overwrite all sectors with random data (Slower, more secure).')
+        print('3. I want to quit.') 
+        choice = input('Select an option (1, 2 or 3): ')
+
+        if choice in ('1', '2', '3'): 
+            return choice 
+
+menu()
+
+'''
+def interactiveMode(): 
+    """ Display menu-driven options and return conversions. """
+    while True: 
+        choice = menu() 
+
+        if choice == '3': 
+            print('3')
+            # sys.exit() 
+        elif choice == '1': 
+            print('1')
+            # zerosToDevPart()
+        elif choice == '2': 
+            print('2')
+            # randomToDevPart() 
+
+def wipeDrive():
+    """ Program to Wipe drive """ 
+    
+    osCheck()
+    interactiveMode()
+    
+if __name__ == '__main__':
+    print(28 * '-', " pyWype ", 28 * '-')
+    print('PYTHON DISK WIPING UTILITY.\nTHIS TOOL WILL IRRECOVERABLY WIPE DATA FROM YOUR DRIVE.\nPROCEED WITH CAUTION.') 
+ 
+    wipeDrive()
+ 
 def numberOfWipes(): 
     """ Prompt user for number of wipes to perform """ 
     
@@ -118,38 +169,7 @@ def randomToDevPart():
         os.system(('dd if=/dev/random |pv --progress --time --rate --bytes| dd of={} bs=4096'.format(append))) # pv -ptrb 
         passes += 1 
 
-def menu(): 
-    """ Menu prompt for use to select program option """ 
-    while True: 
-        print(30 * "-", "MENU", 30 * "-")
-        print('1. Overwrite all sectors with zeros (Faster, less secure).')
-        print('2. Overwrite all sectors with random data (Slower, more secure).')
-        print('3. I want to quit.') 
-        choice = input('Select an option (1, 2 or 3): ')
 
-        if choice in ('1', '2', '3'): 
-            return choice 
+  
 
-def interactiveMode(): 
-    """ Display menu-driven options and return conversions. """
-    while True: 
-        choice = menu() 
-
-        if choice == '3': 
-            sys.exit() 
-        elif choice == '1': 
-            zerosToDevPart()
-        elif choice == '2': 
-            randomToDevPart()   
-
-def wipeDrive():
-    """ Program to Wipe drive """ 
-    
-    osCheck()
-    interactiveMode()
-    
-if __name__ == '__main__':
-    print(28 * '-', " pyWype ", 28 * '-')
-    print('PYTHON DISK WIPING UTILITY.\nTHIS TOOL WILL IRRECOVERABLY WIPE DATA FROM YOUR DRIVE.\nPROCEED WITH CAUTION.') 
- 
-    wipeDrive()
+'''
