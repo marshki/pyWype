ba2a4ba03095ea2f979bd46c8e8ac4646c98a2d3
marshki <marshall@morethantomorrow.org>
2017-08-17 16:30:42 -0400

    possible refactor for processing count

---
+++ note.py
@@ -0,0 +1,16 @@
+#!/bin/py 
+# Simplify randomToDevice, zerosToDevice functions
+
+pass_num = 1
+num = 5 
+
+for pass_num in range(num): 
+    print("Processing pass count {} of {} ...".format(pass_num, num))
+
+'''
+0 5
+1 5
+2 5
+3 5
+4 5
+'''
