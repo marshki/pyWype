4f9aef9b80a929899025aabd5ba57fc67aa0197a
marshki <marshall@morethantomorrow.org>
2017-08-18 16:54:21 -0400

    refactor note.py

--- note.py
+++ note.py
@@ -1,16 +1,17 @@
 #!/bin/py 
 # Simplify randomToDevice, zerosToDevice functions
 
+
+def numWipes(): 
+	""" Number of wipes """ 
+	wipes = int(input("NUmber of wipes? : ")
+	return wipes 
+
+
 pass_num = 1
 num = 5 
 
 for pass_num in range(num): 
     print("Processing pass count {} of {} ...".format(pass_num, num))
 
-'''
-0 5
-1 5
-2 5
-3 5
-4 5
-'''
+
