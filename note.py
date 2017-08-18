4d5d6f412a309649a2eefb350771c07c97022651
marshki <marshall@morethantomorrow.org>
2017-08-18 18:49:49 -0400

    refactor counting function of wipes

--- note.py
+++ note.py
@@ -1,17 +1,18 @@
-#!/bin/py 
+#!/usr/bin/env python 
 # Simplify randomToDevice, zerosToDevice functions
-
+# This seems to work 2017.08.18
 
 def numWipes(): 
 	""" Number of wipes """ 
-	wipes = int(input("NUmber of wipes? : ")
+	wipes = int(input("Number of wipes? : "))
 	return wipes 
 
+def passCount(): 
+    """ Keep track on number of passes performed """
+  
+num  = numWipes()
 
-pass_num = 1
-num = 5 
+for int in range(num): 
+    print("Processing pass count {} of {} ...".format(int + 1, num))
 
-for pass_num in range(num): 
-    print("Processing pass count {} of {} ...".format(pass_num, num))
-
-
+passCount()
