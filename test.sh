8132f6b37ba1e6a48f80c501274af965bf146591
marshki <marshall@morethantomorrow.org>
2018-01-17 12:33:08 -0500

    format date output

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
-#!/bin/bash 
+#!/bin/bash
 
 printf "%s\n" "FQDN: $(hostname -f)"
 
-date "+DATE: %Y-%m-%d%nTIME: %H:%M:%S"
+printf "%s\n" "DATE: $(date +%A-%d-%B-%Y-%R)" 
+
+# $(+DATE: %Y-%m-%d% TIME: %H:%M:%S)"
+
+uptime
