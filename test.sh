20843c12415af6ac992308d82e988663ca68ce46
marshki <marshall@morethantomorrow.org>
2018-01-19 22:15:49 -0500

    add place holders

--- test.sh
+++ test.sh
@@ -6,4 +6,13 @@ printf "%s\n" "DATE: $(date +%_A-%_d-%B-%Y) TIME: $(date +%R)"
 
 printf "%s\n" "UPTIME: $(uptime | tr -d ',' |awk '{print $3, $4, $5}')"
 
+printf "%s\n" "MEMORY: $()" 
 
+printf "%s\n" "DISK USAGE: $()"
+
+printf "%s\n" "LOAD AVERAGES: $()" 
+
+printf "%s\n" "RUNNING PROCESSES: $()" 
+
+printf "%s\n" "IP ADDRESS: $()" 
+ 
