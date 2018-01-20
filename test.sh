76b25e71a946a9640c0badc32c670f4fe2b77cf5
marshki <marshall@morethantomorrow.org>
2018-01-19 21:40:47 -0500

    uptime days hours extraction

--- test.sh
+++ test.sh
@@ -4,4 +4,6 @@ printf "%s\n" "FQDN: $(hostname -f)"
 
 printf "%s\n" "DATE: $(date +%_A-%_d-%B-%Y) TIME: $(date +%R)" 
 
-printf "%s\n" "UPTIME: $(uptime | awk '{print $3, $4, $5}')" 
+printf "%s\n" "UPTIME: $(uptime | tr -d ',' |awk '{print $3, $4, $5}')"
+
+
