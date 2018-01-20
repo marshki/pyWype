052844f43a66530000d00dd0ee4bb87ec63921c9
marshki <marshall@morethantomorrow.org>
2018-01-20 15:52:59 -0500

    add if/else for OS detection to extract RAM info

--- test.sh
+++ test.sh
@@ -2,17 +2,21 @@
 
 printf "%s\n" "FQDN: $(hostname -f)"
 
-printf "%s\n" "DATE: $(date +%_A-%_d-%B-%Y) TIME: $(date +%R)" 
+printf "%s\n" "DATE: $(date +%_A-%_d-%B-%Y) TIME: $(date +%R)"
 
 printf "%s\n" "UPTIME: $(uptime | tr -d ',' |awk '{print $3, $4, $5}')"
 
-printf "%s\n" "MEMORY: $()" 
+if [ "$OSTYPE" == "linux-gnu*" ]; then
+  	printf "%s\n" "MEMORY: $(free -gh)"
+else [ "$OSTYPE" == "darwin*" ];
+	printf "%s\n" "MEMORY: $(top -l 1 -s 0 | grep PhysMem)"
+fi 
+#printf "%s\n" "MEMORY: $()"
 
-printf "%s\n" "DISK USAGE: $()"
+#printf "%s\n" "DISK USAGE: $()"
 
-printf "%s\n" "LOAD AVERAGES: $()" 
+#rintf "%s\n" "LOAD AVERAGES: $()"
 
-printf "%s\n" "RUNNING PROCESSES: $()" 
+#printf "%s\n" "RUNNING PROCESSES: $()"
 
-printf "%s\n" "IP ADDRESS: $()" 
- 
+#printf "%s\n" "IP ADDRESS: $()"
