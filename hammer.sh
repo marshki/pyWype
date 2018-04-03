0c4cd19ec08686759ecde0e7b6ee4e7479fa165b
marshki <marshki@users.noreply.github.com>
2018-04-03 12:09:55 -0400

    Create hammer.sh

---
+++ hammer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# One of the simplest ways to get it to work without escaping any chars with echo.
+cat << "EOF" 
+ ____________________
+/                    \
+|     In case of     |
+|     Frustration    |
+\____________________/
+         !  !
+         !  !
+         L_ !
+        / _)!
+       / /__L
+ _____/ (____)
+        (____)
+ _____  (____)
+      \_(____)
+         !  !
+         !  !
+         \__/
+EOF
