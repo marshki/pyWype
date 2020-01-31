0a8a1771710cb650f0d62fa6853e59f82d29dc94
marshki <marshall@morethantomorrow.org>
2020-01-31 11:46:28 -0500

    rename, add test

---
+++ tests/test_user_is_root.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+def user_is_root():
+    """Check if current UID is 0.
+    """
+
+    if os.getuid() != 0:
+        print("This program requires ROOT privileges. Exiting.")
+        sys.exit()
+
+user_is_root()
